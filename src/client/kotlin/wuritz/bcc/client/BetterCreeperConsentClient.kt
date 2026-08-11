3c8ada2af4527f3d5229078c5b9b57f220fa468b
wuritz <ritzke06@gmail.com>
2026-08-11 12:49:05 +0200

    Bugfix - Left out an else case

--- src/client/kotlin/wuritz/bcc/client/BetterCreeperConsentClient.kt
+++ src/client/kotlin/wuritz/bcc/client/BetterCreeperConsentClient.kt
@@ -20,7 +20,7 @@ object BetterCreeperConsentClient : ClientModInitializer {
 				BetterCreeperConsent.LOG.info("Received open screen packet for creeper {}", creeperId)
 
 				if (!context.client().gui.canInterruptScreen()) ClientPlayNetworking.send(ResponsePayload(creeperId, false, false))
-				context.client().gui.setScreen(ConsentScreen(creeperId))
+				else context.client().gui.setScreen(ConsentScreen(creeperId))
 			}
         }
 
