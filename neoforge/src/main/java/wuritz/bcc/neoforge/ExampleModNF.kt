3f0634cbc0725f4fa6ccf082c554084799fa6f4e
wuritz <ritzke06@gmail.com>
2026-08-20 17:00:36 +0200

    Initial Architectury port, 26.2 - fabric & neoforge

---
+++ neoforge/src/main/java/wuritz/bcc/neoforge/ExampleModNF.kt
@@ -0,0 +1,8 @@
+package wuritz.bcc.neoforge
+
+import net.neoforged.fml.common.Mod
+import wuritz.bcc.ExampleMod
+
+@Mod(ExampleMod.MOD_ID)
+object ExampleModNF {
+}
--- src/main/kotlin/wuritz/bcc/utils/Utils.kt
+++
@@ -1,5 +0,0 @@
-package wuritz.bcc.utils
-
-object Utils {
-    const val EXPLOSION_RADIUS = 10f
-}
